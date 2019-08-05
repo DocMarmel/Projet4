e85123bac2ed3da9980957ac4f6506750ce03629
Maxime <33199920+DocMarmel@users.noreply.github.com>
2019-08-05 19:31:25 +0200

    Passage en contrôleur frontal

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
 
-require 'model/model.php';
+require 'controller/controller.php';
 
-try {
-  $tickets = getTickets();
-  require 'view/home.php';
-
-} catch (Exception $e) {
-  $msgError = $e->getMessage();
-  require 'view/error.php';
+try{
+  if(isset($_GET['action'])){
+    if($_GET['action'] == 'ticket'){
+      if(isset($_GET['id'])){
+        // intval renvoie la valeur numérique du paramètre ou 0 en cas d'échec
+        $idTicket = intval($_GET['id']);
+        if($idTicket != 0){
+          ticket($idTicket);
+        }else{
+          throw new Exception("Idendtifiant de billet non valide");
+        }
+      }else{
+        throw new Exception("Identifiant de billet non défini");
+      }
+    }else{
+      throw new Exception("Action non valide");
+    }
+  }else{
+    home();  // Action par défaut
+  }
+}catch(Exception $e){
+  error($e->mgetMessage());
 }
