70e56684f880fbdd08f66500a746a5e53cb4623e
Maxime <33199920+DocMarmel@users.noreply.github.com>
2019-08-05 18:47:40 +0200

    Ajout fonction commentaires

--- model/model.php
+++ model/model.php
@@ -1,14 +1,35 @@
 <?php
 
-// Renvoie la liste de tous les billets, triés par id décroissant
-function getTickets(){
-  $db = getDb();
-  $tickets = $db->query("SELECT * FROM chapitres ORDER BY id DESC");
-  return $tickets;
-}
-
 // Connexion à la BDD
 function getDb(){
     $db = new PDO('mysql:host=localhost;dbname=blog', 'forteroche', 'J.ForterocheBlog', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     return $db;
+}
+
+// Renvoie la liste de tous les billets, triés par id décroissant
+function getTickets(){
+  $db = getDb();
+  $tickets = $db->query("SELECT * FROM chapitres ORDER BY idChap DESC");
+  return $tickets;
+}
+
+// Renvoie les informations d'un billet
+function getTicket($idTicket){
+  $db = getDb();
+  $ticket = $db->prepare("SELECT * FROM chapitres WHERE idChap=?");
+  $ticket->execute(array($idTicket));
+
+  if($ticket->rowCount() == 1){
+    return $ticket->fetch();
+  }else{
+    throw new Exception("Aucun billet ne correspond à l'identifiant '$ticket'");
+  }
+}
+
+// Renvoie la liste des commentaires du billet
+function getComments($idTicket){
+  $db = getDb();
+  $comments = $db->prepare("SELECT * FROM commentaires WHERE idChap=?");
+  $comments->execute(array($idTicket));
+  return $comments;
 }
