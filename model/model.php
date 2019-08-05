b3c02a03862d1822db97dcfd60eb2186d79f07de
Maxime <33199920+DocMarmel@users.noreply.github.com>
2019-08-05 16:31:15 +0200

    Isolation de la connexion à la BDD + ajout erreur

--- model/model.php
+++ model/model.php
@@ -1,8 +1,14 @@
 <?php
-require 'includes/db_connect.php';
 
 // Renvoie la liste de tous les billets, triés par id décroissant
 function getTickets(){
+  $db = getDb();
   $tickets = $db->query("SELECT * FROM chapitres ORDER BY id DESC");
   return $tickets;
+}
+
+// Connexion à la BDD
+function getDb(){
+    $db = new PDO('mysql:host=localhost;dbname=blog', 'forteroche', 'J.ForterocheBlog', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    return $db;
 }
