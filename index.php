7f764953ec0f2342bd37107e446d5958e52851aa
Maxime <33199920+DocMarmel@users.noreply.github.com>
2019-08-06 13:41:47 +0200

    Maj pour le routeur

--- index.php
+++ index.php
@@ -1,27 +1,6 @@
 <?php
 
-require 'controller/controller.php';
+require 'controller/Router.php';
 
-try{
-  if(isset($_GET['action'])){
-    if($_GET['action'] == 'ticket'){
-      if(isset($_GET['id'])){
-        // intval renvoie la valeur numérique du paramètre ou 0 en cas d'échec
-        $idTicket = intval($_GET['id']);
-        if($idTicket != 0){
-          ticket($idTicket);
-        }else{
-          throw new Exception("Idendtifiant de billet non valide");
-        }
-      }else{
-        throw new Exception("Identifiant de billet non défini");
-      }
-    }else{
-      throw new Exception("Action non valide");
-    }
-  }else{
-    home();  // Action par défaut
-  }
-}catch(Exception $e){
-  error($e->mgetMessage());
-}
+$router = new Router();
+$router->routerRequest();
