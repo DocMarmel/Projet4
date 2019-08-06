49ce7c174edf9b5798f95351495b6beb411d532b
Maxime <33199920+DocMarmel@users.noreply.github.com>
2019-08-06 13:37:35 +0200

    Maj de la nav

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,14 +2,14 @@
   <div class="header">
     <div class="nav">
       <div class="logo">
-        <a href="/blogP4/index.php">
+        <a href="index.php">
           <img src="" alt="logo de l'écrivain">
         </a>
       </div>
       <ul class="menu">
-        <li><a href="/blogP4/index.php">Accueil</a></li>
+        <li><a href="index.php">Accueil</a></li>
         <li><a href="/blogP4/view/chapterList.php">Liste des chapitres</a></li>
-        <li><a href="/blogP4/view/connection.php">Connexion</a></li>
+        <li><a href="index.php?action=connection">Connexion</a></li>
       </ul>
       </div>
     </div>
