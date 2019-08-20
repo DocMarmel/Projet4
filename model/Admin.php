c03273066a911c548e538ddf8fc0891f6f087d77
Maxime <33199920+DocMarmel@users.noreply.github.com>
2019-08-20 12:27:39 +0200

    Ajout suppression + accept com

--- model/Admin.php
+++ model/Admin.php
@@ -13,4 +13,25 @@ class Admin extends Model{
       throw new Exception("Aucun utilisateur ne correspond à ces identifiants");
     }
   }
+
+  public function addChapter($chapter, $titleChap, $contentChap){
+    $sql = "INSERT INTO chapitres (chapter, titleChap, contentChap) VALUES(?,?,?)";
+    $addChap = $this->executeRequest($sql, array($chapter, $titleChap, $contentChap));
+  }
+
+  public function acceptCom($idCom){
+    $sql = "UPDATE commentaires SET report=0 WHERE idCom=?";
+    $acceptCom = $this->executeRequest($sql, array($idCom));
+  }
+
+  public function deleteCom($idCom){
+    $sql = "DELETE FROM commentaires WHERE idCom=?";
+    $deleteCom = $this->executeRequest($sql, array($idCom));
+  }
+
+  public function deleteChap($idChap){
+    $sql = "DELETE FROM chapitres WHERE idChap=?";
+    // "UNION DELETE FROM commentaires WHERE idChap=?";
+    $deleteChap = $this->executeRequest($sql, array($idChap));
+  }
 }
