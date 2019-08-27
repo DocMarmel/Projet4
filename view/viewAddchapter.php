e9312d7d6be572ed8aedee90c882ab778e892cb8
Maxime <33199920+DocMarmel@users.noreply.github.com>
2019-08-27 18:16:58 +0200

    Ajout créer chap

--- view/viewAddchapter.php
+++ view/viewAddchapter.php
@@ -1,11 +1,12 @@
-<?php $this->title = "Billet simple pour l'Alaska - Ajout de chapitre"; ?>
-<br><br>
-  <form method="post">
-    <label for="numChapter">N° du chapitre: </label>
-    <input type="number" name="numChapter" min="1"><br>
-    <label for="chapter">Nom du chapitre: </label>
-    <input type="text" name="chapter"><br>
-    <label for="textareaTinymce">Contenu du chapitre: </label>
-    <textarea name="textareaTinymce" id="textareaTinymce">Next, get a free Tiny Cloud API key!</textarea><br>
-    <input type="submit" name="addChapter" value="Ajouter le chapitre">
-  </form>
+<?php session_start(); ?>
+  <?php $this->title = "Billet simple pour l'Alaska - Ajout de chapitre"; ?>
+  <br><br>
+    <form action="index.php?action=addchap" method="post">
+      <label for="numChapter">N° du chapitre: </label>
+      <input type="number" name="numChapter" min="1"><br>
+      <label for="chapter">Nom du chapitre: </label>
+      <input type="text" name="chapter"><br>
+      <label for="contentChap">Contenu du chapitre: </label>
+      <textarea id="tinymce" name="contentChap"></textarea><br>
+      <input type="submit" name="addChapter" value="Ajouter le chapitre">
+    </form>
