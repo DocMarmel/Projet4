4388599888b4de382c11cf994b9959022d7f63c7
Maxime <33199920+DocMarmel@users.noreply.github.com>
2019-08-27 18:16:03 +0200

    Ajout tinymce

--- view/template.php
+++ view/template.php
@@ -1,8 +1,18 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
 <html lang="fr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=100%; min-scale=1.0; max-scale=1.0">
     <link rel="stylesheet" href="public/css/style.css">
+    <!-- CDN TinyMCE -->
+    <script src="public/tinymce/tinymce.min.js"></script>
+    <script>
+      tinymce.init({
+        selector: '#tinymce',
+      });
+    </script>
+
     <title><?= $title ?></title>
   </head>
   <body>
@@ -14,6 +24,7 @@
         <?= $content ?>
       </div>
       <footer id="footer">
+        <br><br>
         Footer à rajouter !!!
       </footer>
     </div>
