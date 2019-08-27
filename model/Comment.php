26215f79fe1ae7fec7b59762afe0ead534e77b6f
Maxime <33199920+DocMarmel@users.noreply.github.com>
2019-08-27 18:14:27 +0200

    Ajout signalement com

--- model/Comment.php
+++ model/Comment.php
@@ -1,20 +1,36 @@
 <?php
+session_start();
 
 require_once 'model/Model.php';
 
 class Comment extends Model{
 
+
+
   // Renvoie la liste des commentaires du billet
   public function getComments($idChap){
-    $sql = "SELECT * FROM commentaires WHERE idChap=?";
+    $sql = "SELECT * FROM commentaires WHERE idChap=? ORDER BY dateCom DESC";
     $comments = $this->executeRequest($sql, array($idChap));
     return $comments;
   }
 
+  // Renvoie la liste des commentaires pour la page admin
+    public function getCommentsAdmin(){
+      $sql = "SELECT * FROM commentaires ORDER BY report DESC, dateCom DESC";
+      $comments = $this->executeRequest($sql);
+      return $comments;
+    }
+
   // Ajout d'un commentaire
   public function addComment($authorCom, $contentCom, $idChap){
-    $sql = "INSERT INTO commentaires(dateCom, authorCom, contentCom, idChap) VALUES(?, ?, ?, ?)";
-    $dateCom = date(DATE_W3C);
-    $this->executeRequest($sql, array($dateCom, $authorCom, $contentCom, $idChap));
+    $sql = "INSERT INTO commentaires(dateCom, authorCom, contentCom, idChap) VALUES(NOW(),?,?,?)";
+    $this->executeRequest($sql, array($authorCom, $contentCom, $idChap));
+  }
+
+  // Signaler un commentaire
+  public function report($idCom){
+    $sql = "UPDATE commentaires SET report = report +1 WHERE idCom=?";
+    $newReport = $this->executeRequest($sql, array($idCom));
+    return $newReport;
   }
 }
